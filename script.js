ae99910a76e928fd1ca7bde610cb2bb8c98422fb
rihanazar1 <rihanazhar5@gmail.com>
2024-12-02 13:28:16 +0530

    instagram like feature

---
+++ script.js
@@ -0,0 +1,125 @@
+var usersData = [
+    {
+        "username": "travel_diaries",
+        "userlocation": "Paris, France",
+        "userprofile": "https://images.unsplash.com/photo-1524638431109-93d95c968f03?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "userPost": "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=3546&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "like": true,
+        "likeCount": 200,
+        "commentCount": 45,
+        "shareCount": 30,
+        "caption": "Enjoying the sunset at the Eiffel Tower. #ParisVibes #TravelGoals",
+        "timeAgoUploaded": 5
+    },
+    {
+        "username": "foodie_lover",
+        "userlocation": "Mumbai, India",
+        "userprofile": "https://images.unsplash.com/photo-1487069838269-7c05365b400b?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "userPost": "https://images.unsplash.com/photo-1554978991-33ef7f31d658?q=80&w=2620&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "like": false,
+        "likeCount": 530,
+        "commentCount": 20,
+        "shareCount": 12,
+        "caption": "The best vada pav I've ever had! 🌮❤️ #MumbaiStreetFood",
+        "timeAgoUploaded": 12
+    },
+    {
+        "username": "fitness_freak",
+        "userlocation": "New York, USA",
+        "userprofile": "https://images.unsplash.com/photo-1541534741688-6078c6bfb5c5?q=80&w=3538&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "userPost": "https://images.unsplash.com/photo-1485727749690-d091e8284ef3?q=80&w=3532&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "like": true,
+        "likeCount": 800,
+        "commentCount": 60,
+        "shareCount": 25,
+        "caption": "Start your day with some yoga. #FitnessGoals #HealthyLiving",
+        "timeAgoUploaded": 24
+    },
+    {
+        "username": "artsy_corner",
+        "userlocation": "London, UK",
+        "userprofile": "https://images.unsplash.com/photo-1647205552933-12e3dfdb77b5?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "userPost": "https://plus.unsplash.com/premium_photo-1692650759141-ed71d789ef1b?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "like": true,
+        "likeCount": 950,
+        "commentCount": 78,
+        "shareCount": 40,
+        "caption": "New piece in the gallery. Let me know your thoughts! 🎨 #AbstractArt",
+        "timeAgoUploaded": 7
+    },
+    {
+        "username": "adventure_seeker",
+        "userlocation": "Kyoto, Japan",
+        "userprofile": "https://images.unsplash.com/photo-1681562978415-bbf1dfe59353?q=80&w=2624&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "userPost": "https://images.unsplash.com/photo-1545569341-9eb8b30979d9?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "like": false,
+        "likeCount": 600,
+        "commentCount": 33,
+        "shareCount": 18,
+        "caption": "The serenity of Kyoto's bamboo forest. 🌳 #NatureLover",
+        "timeAgoUploaded": 18
+    }
+];
+
+var wrapper = document.querySelector(".wrapper");
+function mainFunction(){
+var clutter = " ";
+usersData.forEach(function(dets, idx){
+    clutter += ` <div class="instagram-post">
+            <header>
+                <img src="${dets.userprofile}">
+                <div class="profile-info">
+                    <h2>${dets.username}</h2>
+                    <span>Gibran Alcocer · Travel</span>
+                </div>
+                <div class="follow-btn">Follow</div>
+            </header>
+            <div class="image-container">
+                <img  src="${dets.userPost}" alt="">
+            </div>
+            <div class="content">
+                <div class="icons">
+                    <div class="left-icons">
+                        <span id="${idx}">${dets.like? '<i id="liked" class="ri-heart-fill"></i>' : '<i id="notliked" class="ri-heart-line"></i>'}</span>
+                        <i class="ri-chat-3-line"></i>
+                        <i class="ri-share-line"></i>
+                    </div>
+                    <div class="right-icons">
+                        <i class="ri-save-line"></i>
+                    </div>
+                </div>
+                <div class="insight">
+                    <div class="likes">${dets.likeCount}</div>
+                    <div class="comment">${dets.commentCount}</div>
+                    <div class="share">${dets.shareCount}</div>
+                </div>
+                <div class="description">
+                     ${dets.caption}
+                </div>
+                <div class="time">${dets.timeAgoUploaded} days ago</div>
+            </div>
+        </div>`
+
+
+wrapper.innerHTML = clutter;
+})
+};
+
+mainFunction();
+
+wrapper.addEventListener("click",function(dets){
+    
+    if(usersData[dets.target.id].like){
+        usersData[dets.target.id].likeCount--;
+        usersData[dets.target.id].like = false;
+    }else{
+        usersData[dets.target.id].likeCount++;
+        usersData[dets.target.id].like = true;
+    }
+    mainFunction()
+    
+})
+
+
+
+
